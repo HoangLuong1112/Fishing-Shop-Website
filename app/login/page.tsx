cd9d358fbb0de2c187fead5e41b67df724d46005
THLuong <110144291+HoangLuong1112@users.noreply.github.com>
2026-03-23 21:57:27 +0700

    First Upload

---
+++ app/login/page.tsx
@@ -0,0 +1,46 @@
+export default function Login() {
+    return (
+        <div className="bg-gray-200 h-screen flex items-center justify-center">
+
+            <div className="w-[1000px] h-[500px] m-4 flex flex-row justify-between border-2 border-blue-500 bg-gray-400">
+                <div className="bg-green-400">
+                    wge
+                </div>
+                <div className="bg-green-400">
+                    gew
+                </div>
+            </div>
+
+            {/* <div className="w-225 h-112.5 flex border-2 border-blue-500 bg-gray-400">
+                
+                <div className="w-[35%] bg-red-500"></div>
+
+                <div className="w-[65%] flex items-center justify-center">
+                    <div className="w-[70%] flex flex-col gap-4">
+                        
+                        <label className="text-lg">tên đăng nhập</label>
+                        <input className="h-11 bg-gray-100 px-3 outline-none" />
+
+                        <label className="text-lg">mật khẩu</label>
+                        <input type="password" className="h-11 bg-gray-100 px-3 outline-none" />
+
+                        <div className="flex items-center text-sm mt-1">
+                        <input type="checkbox" />
+                        <span className="ml-2">Ghi nhớ đăng nhập</span>
+                        </div>
+
+                        <button className="h-14 bg-lime-400 text-2xl hover:bg-lime-500 transition">
+                            Đăng nhập
+                        </button>
+
+                        <button className="h-14 bg-lime-400 text-2xl hover:bg-lime-500 transition">
+                            Đăng ký
+                        </button>
+
+                    </div>
+                </div>
+
+            </div> */}
+        </div>
+    )
+}
