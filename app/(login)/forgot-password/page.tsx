b7a7980c3e5da41ce4614ec8e57361eea6643d4a
THLuong <110144291+HoangLuong1112@users.noreply.github.com>
2026-03-26 21:56:41 +0700

    Update page.tsx

--- app/(login)/forgot-password/page.tsx
+++ app/(login)/forgot-password/page.tsx
@@ -5,25 +5,17 @@ export default function ForgotPassword() {
             <form className="w-200 h-auto m-4 border-4 border-white rounded-lg overflow-hidden">
                 
                 <div className="bg-[#C1E6FF] flex flex-col gap-4 p-4">
-                    <p className="text-4xl font-bold py-5">Tạo tài khoản mới</p>
+                    <p className="text-4xl font-bold py-5">Quên mật khẩu</p>
                     <div>
-                        <label className="text-lg">Email</label>
+                        <label className="text-lg">Nhập Email của bạn</label>
                         <input type="email" required className="text-lg bg-white h-11 w-full" />
-                    </div>
-                    <div>
-                        <label className="text-lg">Tên đăng nhập</label>
-                        <input required className="text-lg bg-white h-11 w-full" />
-                    </div>
-                    <div>
-                        <label className="text-lg">Mật khẩu</label>
-                        <input type="password" required className="text-lg bg-white h-11 w-full" />
                     </div>
 
                     <div>
                         <p>Captcha</p>
                     </div>
 
-                    <button className="h-12 text-2xl bg-blue-400 hover:bg-blue-500 transition">Đăng ký</button>
+                    <button className="h-12 text-2xl bg-blue-400 hover:bg-blue-500 transition">Gửi yêu cầu</button>
                     
                 </div>
 
