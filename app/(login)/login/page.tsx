11ad645697eace4e1bdd836b6bfa8375f35bef48
THLuong <110144291+HoangLuong1112@users.noreply.github.com>
2026-03-27 21:24:59 +0700

    Update login page

--- app/(login)/login/page.tsx
+++ app/(login)/login/page.tsx
@@ -5,7 +5,7 @@ export default function Login() {
     return (
         <div className="bg-[#38384C] h-screen flex items-center justify-center">
 
-            <form className="w-250 h-130 m-4 grid md:grid-cols-2 bg-white border-4 border-white rounded-lg overflow-hidden">
+            <form className="w-250 h-auto m-4 grid md:grid-cols-2 bg-white border-4 border-white rounded-lg overflow-hidden">
                 <div className="relative w-full h-full">
                     <Image 
                         alt="login-picture" 
@@ -34,7 +34,9 @@ export default function Login() {
                     </div>
 
                     <button className="h-12 text-2xl bg-blue-400 hover:bg-blue-500 transition">Đăng nhập</button>
-                    <button className="h-12 text-2xl bg-blue-400 hover:bg-blue-500 transition">Đăng ký</button>
+                    <Link href="/create-new-account" className="h-12 text-2xl bg-blue-400 hover:bg-blue-500 transition flex items-center justify-center">
+                        Đăng ký
+                    </Link>
                     
                     <Link href="/forgot-password" className="text-sm text-blue-500 hover:text-blue-700 underline">
                         Quên mật khẩu?
