7f44c6daa4d89268fc12d033d6d73e4540d28256
THLuong <110144291+HoangLuong1112@users.noreply.github.com>
2026-04-19 09:59:23 +0700

    finish basic crud for product

--- app/(manager)/manager/products/[productId]/ProductForm.tsx
+++ app/(manager)/manager/products/[productId]/ProductForm.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { addProduct, updateProduct } from "@/app/actions/productAction";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
+import { createClient } from "@/utils/supabase/client";
 
 export default function ProductForm({ initialData, categories =[], isEdit }: any) {
     const router = useRouter();
@@ -17,6 +18,11 @@ export default function ProductForm({ initialData, categories =[], isEdit }: any
         status: initialData?.status ?? true,
         id_category: initialData?.id_category || "",
     });
+
+    const [newImage, setNewImage] = useState<File | null>(null);
+    const [previewImage, setPreviewImage] = useState(
+        initialData?.image_url || ""
+    );
 
     const [loading, setLoading] = useState(false);
 
@@ -34,15 +40,76 @@ export default function ProductForm({ initialData, categories =[], isEdit }: any
         }));
     };
 
+    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (file) {
+            setNewImage(file);
+            setPreviewImage(URL.createObjectURL(file));
+        }
+    };
+
     const handleSubmit = async (e: any) => {
         e.preventDefault();
         setLoading(true);
 
         try {
+            const supabase = createClient();
+            let imageUrl = form.image_url;
+            if (newImage) { // ảnh mới
+                const fileExt = newImage.name.split(".").pop() || "png";
+                const fileName = `products/${initialData?.id}-${Date.now()}.${fileExt}`;
+
+                // validate
+                if (newImage.size > 2 * 1024 * 1024) {
+                    toast.error("Ảnh tối đa 2MB");
+                    return;
+                }
+
+                if (!newImage.type.startsWith("image/")) {
+                    toast.error("Chỉ được upload ảnh");
+                    return;
+                }
+
+                // upload
+                const { error: uploadError } = await supabase.storage
+                    .from("main")
+                    .upload(fileName, newImage);
+
+                if (uploadError) throw uploadError;
+
+                // lấy public url
+                const { data } = supabase.storage
+                    .from("main")
+                    .getPublicUrl(fileName);
+
+                const newUrl = data.publicUrl;
+
+                // xóa ảnh cũ nếu edit
+                if (isEdit && form.image_url) {
+                    console.log("OLD URL:", form.image_url);
+                    const oldPath = form.image_url
+                        ?.split("/storage/v1/object/public/main/")[1]
+                        ?.split("?")[0];
+                    console.log("OLD URL:", imageUrl);
+                    console.log("OLD PATH:", oldPath);
+
+                    if (oldPath) {
+                        await supabase.storage.from("main").remove([oldPath]);
+                    }
+                }
+
+                imageUrl = newUrl;
+            }
+
+            const payload = {
+                ...form,
+                image_url: imageUrl,
+            };
+
             if (isEdit) {
-                await updateProduct(initialData.id, form);
+                await updateProduct(initialData.id, payload);
             } else {
-                await addProduct(form);
+                await addProduct(payload);
             }
 
             router.push("/manager/products");
@@ -78,13 +145,42 @@ export default function ProductForm({ initialData, categories =[], isEdit }: any
                 <input type="number" name="stock_quantity" value={form.stock_quantity} onChange={handleChange} placeholder="Kho" className="w-full border border-black p-2 rounded" />
             </div>
 
-            <input name="image_url" value={form.image_url} onChange={handleChange} placeholder="Ảnh URL" className="w-full border p-2 rounded" />
+            {/* <input name="image_url" value={form.image_url} onChange={handleChange} placeholder="Ảnh URL" className="w-full border p-2 rounded" /> */}
+            <div className="flex items-center gap-4">
+                <p className="font-bold text-lg">Ảnh sản phẩm</p>
+
+                <div className="relative group w-24 h-24">
+                    <img
+                        src={previewImage || "/image/default.png"}
+                        className="w-24 h-24 object-cover rounded border"
+                    />
+
+                    {/* overlay */}
+                    <div className="absolute inset-0 bg-black/40 opacity-0 group-hover:opacity-100 flex items-center justify-center rounded transition">
+                        <button
+                            type="button"
+                            onClick={() => document.getElementById("productImageInput")?.click()}
+                            className="bg-white p-2 rounded-full"
+                        >
+                            📷
+                        </button>
+                    </div>
+
+                    <input
+                        id="productImageInput"
+                        type="file"
+                        accept="image/*"
+                        onChange={handleImageChange}
+                        className="hidden"
+                    />
+                </div>
+            </div>
 
             <select
                 name="id_category"
                 value={form.id_category}
                 onChange={handleChange}
-                className="w-full border p-2 rounded"
+                className="w-full border border-black p-2 rounded"
             >
                 <option value="">-- Chọn danh mục --</option>
                 {categories.map((c: any) => (
